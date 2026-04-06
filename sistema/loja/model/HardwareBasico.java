0e13b06ff478ce6b7d62d633ddfc831213ce17c8
GuiFroes <guilherme9froes@gmail.com>
2026-04-06 01:29:04 -0300

    Refatora código com pacotes, remove ArrayList, adiciona classe Pedido e preços por matrícula

---
+++ sistema/loja/model/HardwareBasico.java
@@ -0,0 +1,22 @@
+package sistema.loja.model;
+
+/**
+ * Representa um componente básico de hardware.
+ */
+public class HardwareBasico {
+    private String nome;
+    private float capacidade;
+
+    public HardwareBasico(String nome, float capacidade) {
+        this.nome = nome;
+        this.capacidade = capacidade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public float getCapacidade() {
+        return capacidade;
+    }
+}
---
+++ sistema/loja/model/SistemaOperacional.java
@@ -0,0 +1,22 @@
+package sistema.loja.model;
+
+/**
+ * Representa o sistema operacional de um computador.
+ */
+public class SistemaOperacional {
+    private String nome;
+    private int tipo; // 32 ou 64 bits
+
+    public SistemaOperacional(String nome, int tipo) {
+        this.nome = nome;
+        this.tipo = tipo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getTipo() {
+        return tipo;
+    }
+}
